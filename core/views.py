7240734329933a0ead1e7b9e8fcf76892b4c732d
wookkl <wjddnr315@gmail.com>
2020-11-20 15:20:54 +0900

    lastestview page

--- core/views.py
+++ core/views.py
@@ -8,7 +8,6 @@ from books import models as book_models
 
 
 def HomeView(request):
-    page = request.GET.get("page", 1)
     movie_querysets = movie_models.Movie.objects.all().order_by("pk")
     book_querysets = book_models.Book.objects.all().order_by("pk")
     person_querysets = person_models.Person.objects.all().order_by("pk")
@@ -16,23 +15,16 @@ def HomeView(request):
         movie_paginator = Paginator(movie_querysets, 10, orphans=5)
         book_paginator = Paginator(book_querysets, 10, orphans=5)
         person_paginator = Paginator(person_querysets, 10, orphans=5)
-        movies = movie_paginator.page(page)
-        books = book_paginator.page(page)
-        people = person_paginator.page(page)
-        prev_page = (
-            movies.has_previous() or books.has_previous() or people.has_previous()
-        )
-        next_page = movies.has_next() or books.has_next() or people.has_next()
+        movies = movie_paginator.page(1)
+        books = book_paginator.page(1)
+        people = person_paginator.page(1)
         return render(
             request,
             "home.html",
             {
-                "movie_page": movies,
-                "book_page": books,
-                "person_page": people,
-                "current_page": page,
-                "has_prev_page": prev_page,
-                "has_next_page": next_page,
+                "movies": movies,
+                "books": books,
+                "people": people,
             },
         )
     except InvalidPage:
